c3bdf2720bd948756c761efa0360442491810800
Rami <ramieljem@gmail.com>
2023-02-09 19:52:06 +0200

    Adding react helmet for meta data

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider>
+      <Helmet>
+        <title>DV-r3f</title>
+        <meta name='description' content='Data visualisation with r3f.' />
+        {/* <link rel="canonical" href="https://www.tacobell.com/" /> */}
+      </Helmet>
+      <App /> 
+    </HelmetProvider>
   </React.StrictMode>,
 )
